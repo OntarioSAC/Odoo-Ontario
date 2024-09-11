3eb259d3c90aee2b0c468e10edc6d56095868fce
OntarioSAC <soporte@inmobiliariaontario.com>
2024-09-11 12:42:12 -0500

    Tu mensaje de commit aquí

---
+++ addons-customize/Ontario24/Ontario/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': "Ontario",
+    'author': "OntarioSAC",
+    'Sumary': "Modulo Ontario",
+    'depends': [],
+    'data': [],
+    'installable': True,
+    'application': True,
+    'auto_installable': False 
+}
